c512e05a761d97c5e0825be9f9794b101d0eb013
TMD <kk412027247@gmail.com>
2019-02-19 01:01:13 +0800

    set up ios

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
+} from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+// noinspection JSAnnotator
+export default class App extends Component<Props> {
+
+  componentDidMount() {
+    // 组件加载完毕之后，隐藏启动页
+    SplashScreen.hide();
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          backgroundColor={'#4f6d7a'}
+          barStyle={'light-content'}
+        />
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#4f6d7a',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#f5fcff',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#f5fcff',
+    marginBottom: 5,
+  },
+});
